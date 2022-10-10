f9768e4f782269c7893c7072f8ca415b9e5ded9e
MathGuiMachDev <MathGuiMachDev@gmail.com>
2022-10-10 17:39:05 -0300

    Change language function

---
+++ scripts/language.js
@@ -0,0 +1,284 @@
+
+var pt_BR = {
+  "navbar":{
+      "title":"Curriculum Vitae",
+      "item-presentation":"Sobre Mim",
+      "item-characteristics":"Características",
+      "item-experience":"Experiência",
+      "item-technologies":"Tecnologias"
+  },
+  "offcanvas":{
+      "title":"Configurações",
+      "color-scheme":"Tema de Cores",
+      "languages":"Idiomas"
+  },
+  "presentation":{
+      "name":"Matheus Guimarães Machado",
+      "professional-title":"Desenvolvedor Java Web Pleno",
+      "email":"MathGuiMachDev@Gmail.com",
+      "phone-number":"+55 (22) 99941 3025",
+      "github-link":"Github.com/MathGuiMachDev"
+  },
+  "personal-info":{
+      "presentation":{
+          "title":"APRESENTAÇÃO",
+          "first-paragraph":"Busco construir carreira em desenvolvimento de software. Gosto de programar seguindo padrões de projeto pré-estabelecidos e manter a integridade e qualidade do código a longo termo.",
+          "second-paragraph":"Sempre estou fazendo cursos para me manter atualizado sobre novas tecnologias e o “estado da arte” atual. Estou disposto a aprender qualquer tecnologia para participar de projetos de desenvolvimento de sistemas.",
+          "third-paragraph":"Cheguei ao ponto da minha vida onde quero me estabilizar profissionalmente e contribuir para o campo profissional que sempre me fascinou tanto."
+      },
+      "personal-data":{
+          "title":"DADOS PESSOAIS",
+          "birth-date":"Data de Nascimento: 03/03/1996",
+          "nationality":"Nacionalidade: Brasileiro",
+          "sex":"Sexo: Masculino",
+          "marital-status":"Estado Civil: Solteiro",
+          "address":"Endereço: Armação dos Búzios, RJ, Brasil"
+      }
+  },
+  "characteristics":[
+      {"title":"Autodidata","description":"(Udemy, Alura, Data Science Academy)"},
+      {"title":"Jogador de Equipe","description":"Me dou bem com trabalho coordenado de equipe. Seja seguindo ou liderando."},
+      {"title":"Adaptabilidade","description":"Aprendizado rápido. Me acostumo facilmente com mudanças de projeto, escopo ou ambiente."},
+      {"title":"Atitude Positiva","description":"Preocupação? Somente com o sucesso do projeto!"},
+      {"title":"Aspirante a Desenvolvedor FullStack","description":"Experiência com Front-end, Back-end, APIs, Webservices, Sistemas locais e distribuídos."}
+  ],
+  "experience":{
+      "work":{
+          "title":"EXPERIÊNCIA",
+          "itens":[
+              {"company-name":"Basis - Tecnologia da Informação, 2021-2022. (Remoto)","job-description":"Desenvolvedor Java. Trabalhei no projeto de um sistema distribuído de protocolo eletrônico de documentos e um sistema integrador de expedição de documentos."},
+              {"company-name":"SD Informática - Automação Comercial, 2018-2020.","job-description":"Desenvolvedor de Software. Trabalhei em projetos de pontos de vendas, emissão de NFC-e, comanda eletrônica, fazendo melhorias e dando manutenção a outros sistemas relacionados."},
+              {"company-name":"Safe Monitoramento, 2018.","job-description":"Técnico de Informática. Configuração de central de monitoramento e instalação de câmeras e outros hardwares de segurança."}
+          ]
+      },
+      "education":{
+          "title":"FORMAÇÃO",
+          "itens":[
+              {"course-title":"Bacharelado em Análise de Sistemas de Informação;","institution":"Universidade Estácio de Sá - Campus Cabo Frio – RJ, 2017;","commentary":"Desenvolvi um jogo como parte do meu trabalho de conclusão de curso: "},
+              {"course-title":"Java COMPLETO Programação Orientada a Objetos + Projetos;","institution":"Dr. Nelio Alves - Udemy, 2020.","commentary":""},
+              {"course-title":"The Complete 2022 Web Development Bootcamp;","institution":"Dr. Angela Yu - Udemy, 2022.","commentary":""}
+          ]
+      },
+      "knowledge":{
+          "title":"CONHECIMENTO",
+          "language":{
+              "title":"LÍNGUAS",
+              "itens":[
+                  "Português (Fluente) (Linguagem Nativa)",
+                  "Inglês (Avançado)"
+              ]
+          },
+          "main-skill":{
+              "title":"PRINCIPAL",
+              "itens":[
+                  "Java (SpringBoot + Hibernate, Restful APIs)",
+                  "Bancos de Dados (MySQL, PostgreSQL, SQLite, MongoDB)",
+                  "HTML5, CSS (BootStrap4), JavaScript (TypeScript, jQuery)",
+                  "Git, GitHub, GitLab"
+              ]
+          },
+          "complementary-skill":{
+              "title":"COMPLEMENTAR",
+              "itens":[
+                  "C# (Unity Game Engine)",
+                  "C++ (Conhecimento Geral)",
+                  "Docker (Gerenciamento de Containers)",
+                  "Jenkins (Administração)",
+                  "Jira (Gerenciamento de tarefas)",
+                  "Heroku (Hospedagem de Web service)"
+              ]
+          }
+      }
+  },
+  "technologies":"",
+  "footer":"© Copyright 2022 - Matheus Guimarães Machado"
+};
+
+var en_US = {
+  "navbar":{
+      "title":"Curriculum Vitae",
+      "item-presentation":"About Me",
+      "item-characteristics":"Characteristics",
+      "item-experience":"Experience",
+      "item-technologies":"Technologies"
+  },
+  "offcanvas":{
+      "title":"Configurations",
+      "color-scheme":"Color Scheme",
+      "languages":"Languages"
+  },
+  "presentation":{
+      "name":"Matheus Guimarães Machado",
+      "professional-title":"Java Web Software Developer",
+      "email":"MathGuiMachDev@Gmail.com",
+      "phone-number":"+55 (22) 99941 3025",
+      "github-link":"Github.com/MathGuiMachDev"
+  },
+  "personal-info":{
+      "presentation":{
+          "title":"PRESENTATION",
+          "first-paragraph":"I am looking to build a career in software development. I like to program according to pre-established design patterns and maintain the integrity and quality of the code in the long term.",
+          "second-paragraph":"I'm always taking courses to keep myself updated on new technologies and the current “state of the art”. I am willing to learn any technology to  be able to participate in new projects.",
+          "third-paragraph":"I've reached the point in my life where I want to establish myself professionally and contribute to the professional field that has always fascinated me so much."
+      },
+      "personal-data":{
+          "title":"PERSONAL DATA",
+          "birth-date":"Date of Birth: 03/03/1996",
+          "nationality":"Nationality: Brazilian",
+          "sex":"Sex: Male",
+          "marital-status":"Marital Status: Unmarried",
+          "address":"Address: Armação dos Búzios, RJ, Brasil"
+      }
+  },
+  "characteristics":[
+      {"title":"Mostly Self-Taught","description":"(Udemy, Alura, Data Science Academy)"},
+      {"title":"Team Player","description":"Leading or following I enjoy coordinated team work."},
+      {"title":"Adaptability","description":"Fast Learner. Adapt to changes in project, scope or environment."},
+      {"title":"Positive Attitude","description": "No worries! I only care about the project's success!"},
+      {"title":"Aspiring FullStack Developer","description": "Experienced in Front-end, Back-end, APIs, Webservices, Local and Distributed Systems"}
+  ],
+  "experience":{
+      "work":{
+          "title":"EXPERIENCE",
+          "itens":[
+              {"company-name":"Basis - Tecnologia da Informação, 2021-2022. (Remoto)","job-description":"Java Developer. Worked on a decentralized electronic document protocol system and an integrator system for dispatching said documents between branches of the system."},
+              {"company-name":"SD Informática - Automação Comercial, 2018-2020.","job-description":"C++ Developer. Was in charge of code maintenance and development of new functionalities for a point-of-sale system with electronic invoices and other integrated systems."},
+              {"company-name":"Safe Monitoramento, 2018.","job-description":"IT Technician. Set up a monitoring center and did the installation of cameras and other security hardware."}
+          ]
+      },
+      "education":{
+          "title":"EDUCATION",
+          "itens":[
+              {"course-title":"Bachelor in Information Systems Analysis;","institution":"Universidade Estácio de Sá - Campus Cabo Frio – RJ, 2017;","commentary":"I Developed a game as part of my final papers: "},
+              {"course-title":"Java COMPLETO Programação Orientada a Objetos + Projetos;","institution":"Dr. Nelio Alves - Udemy, 2020.","commentary":""},
+              {"course-title":"The Complete 2022 Web Development Bootcamp;","institution":"Dr. Angela Yu - Udemy, 2022.","commentary":""}
+          ]
+      },
+      "knowledge":{
+          "title":"KNOWLEDGE",
+          "language":{
+              "title":"LANGUAGE",
+              "itens":[
+                  "Portuguese (Fluent) (Native Language)",
+                  "English (Advanced)"
+              ]
+          },
+          "main-skill":{
+              "title":"MAINS SKILLS",
+              "itens":[
+                  "Java (SpringBoot + Hibernate, Restful APIs)",
+                  "Databases  (MySQL, PostgreSQL, SQLite, MongoDB)",
+                  "HTML5, CSS (BootStrap4), JavaScript (TypeScript, jQuery)",
+                  "Git, GitHub, GitLab"
+              ]
+          },
+          "complementary-skill":{
+              "title":"COMPLEMENTARY SKILLS",
+              "itens":[
+                  "C# (Unity Game Engine)",
+                  "C++ (General Knowledge)",
+                  "Docker (Container Management)",
+                  "Jenkins (Administration)",
+                  "Jira (Tasks Management)",
+                  "Heroku (Web service Hosting)"
+              ]
+          }
+      }
+  },
+  "technologies":"",
+  "footer":"© Copyright 2022 - Matheus Guimarães Machado"
+};
+
+function detectBrowserLanguage(){
+  return navigator.language || navigator.userLanguage; 
+}
+
+function changeLanguage(lang) {
+  $("#_navbar_title").text(lang["navbar"]["title"]);
+  $("#_navbar_item-presentation").text(lang["navbar"]["item-presentation"]);
+  $("#_navbar_item-characteristics").text(lang["navbar"]["item-characteristics"]);
+  $("#_navbar_item-experience").text(lang["navbar"]["item-experience"]);
+  $("#_navbar_item-technologies").text(lang["navbar"]["item-technologies"]);
+  $("#_offcanvas_title").text(lang["offcanvas"]["title"]);
+  $("#_offcanvas_color-scheme").text(lang["offcanvas"]["color-scheme"]);
+  $("#_offcanvas_languages").text(lang["offcanvas"]["languages"]);
+  $("#_presentation_name").text(lang["presentation"]["name"]);
+  $("#_presentation_professional-title").text(lang["presentation"]["professional-title"]);
+  $("#_presentation_email").text(lang["presentation"]["email"]);
+  $("#_presentation_phone-number").text(lang["presentation"]["phone-number"]);
+  $("#_presentation_github-link").text(lang["presentation"]["github-link"]);
+  $("#_personal-info_presentation_title").text(lang["personal-info"]["presentation"]["title"]);
+  $("#_personal-info_presentation_first-paragraph").text(lang["personal-info"]["presentation"]["first-paragraph"]);
+  $("#_personal-info_presentation_second-paragraph").text(lang["personal-info"]["presentation"]["second-paragraph"]);
+  $("#_personal-info_presentation_third-paragraph").text(lang["personal-info"]["presentation"]["third-paragraph"]);
+  $("#_personal-info_personal-data_title").text(lang["personal-info"]["personal-data"]["title"]);
+  $("#_personal-info_personal-data_birth-date").text(lang["personal-info"]["personal-data"]["birth-date"]);
+  $("#_personal-info_personal-data_nationality").text(lang["personal-info"]["personal-data"]["nationality"]);
+  $("#_personal-info_personal-data_sex").text(lang["personal-info"]["personal-data"]["sex"]);
+  $("#_personal-info_personal-data_marital-status").text(lang["personal-info"]["personal-data"]["marital-status"]);
+  $("#_personal-info_personal-data_address").text(lang["personal-info"]["personal-data"]["address"]);
+  $("#_characteristics_0_title").text(lang["characteristics"][0]["title"]);
+  $("#_characteristics_0_description").text(lang["characteristics"][0]["description"]);
+  $("#_characteristics_1_title").text(lang["characteristics"][1]["title"]);
+  $("#_characteristics_1_description").text(lang["characteristics"][1]["description"]);
+  $("#_characteristics_2_title").text(lang["characteristics"][2]["title"]);
+  $("#_characteristics_2_description").text(lang["characteristics"][2]["description"]);
+  $("#_characteristics_3_title").text(lang["characteristics"][3]["title"]);
+  $("#_characteristics_3_description").text(lang["characteristics"][3]["description"]);
+  $("#_characteristics_4_title").text(lang["characteristics"][4]["title"]);
+  $("#_characteristics_4_description").text(lang["characteristics"][4]["description"]);
+  $("#_experience_work_title").text(lang["experience"]["work"]["title"]);
+  $("#_experience_work_0_company-name").text(lang["experience"]["work"]["itens"][0]["company-name"]);
+  $("#_experience_work_0_job-description").text(lang["experience"]["work"]["itens"][0]["job-description"]);
+  $("#_experience_work_1_company-name").text(lang["experience"]["work"]["itens"][1]["company-name"]);
+  $("#_experience_work_1_job-description").text(lang["experience"]["work"]["itens"][1]["job-description"]);
+  $("#_experience_work_2_company-name").text(lang["experience"]["work"]["itens"][2]["company-name"]);
+  $("#_experience_work_2_job-description").text(lang["experience"]["work"]["itens"][2]["job-description"]);
+  $("#_experience_education_title").text(lang["experience"]["education"]["title"]);
+  $("#_experience_education_0_course-title").text(lang["experience"]["education"]["itens"][0]["course-title"]);
+  $("#_experience_education_0_institution").text(lang["experience"]["education"]["itens"][0]["institution"]);
+  $("#_experience_education_0_commentary").text(lang["experience"]["education"]["itens"][0]["commentary"]);
+  $("#_experience_education_1_course-title").text(lang["experience"]["education"]["itens"][1]["course-title"]);
+  $("#_experience_education_1_institution").text(lang["experience"]["education"]["itens"][1]["institution"]);
+  $("#_experience_education_1_commentary").text(lang["experience"]["education"]["itens"][1]["commentary"]);
+  $("#_experience_education_2_course-title").text(lang["experience"]["education"]["itens"][2]["course-title"]);
+  $("#_experience_education_2_institution").text(lang["experience"]["education"]["itens"][2]["institution"]);
+  $("#_experience_education_2_commentary").text(lang["experience"]["education"]["itens"][2]["commentary"]);
+  $("#_experience_knowledge_title").text(lang["experience"]["knowledge"]["title"]);
+  $("#_experience_knowledge_language_title").text(lang["experience"]["knowledge"]["language"]["title"]);
+  $("#_experience_knowledge_language_0").text(lang["experience"]["knowledge"]["language"]["itens"][0]);
+  $("#_experience_knowledge_language_1").text(lang["experience"]["knowledge"]["language"]["itens"][1]);
+  $("#_experience_knowledge_main-skill_title").text(lang["experience"]["knowledge"]["main-skill"]["title"]);
+  $("#_experience_knowledge_main-skill_0").text(lang["experience"]["knowledge"]["main-skill"]["itens"][0]);
+  $("#_experience_knowledge_main-skill_1").text(lang["experience"]["knowledge"]["main-skill"]["itens"][1]);
+  $("#_experience_knowledge_main-skill_2").text(lang["experience"]["knowledge"]["main-skill"]["itens"][2]);
+  $("#_experience_knowledge_main-skill_3").text(lang["experience"]["knowledge"]["main-skill"]["itens"][3]);
+  $("#_experience_knowledge_complementary-skill_title").text(lang["experience"]["knowledge"]["complementary-skill"]["title"]);
+  $("#_experience_knowledge_complementary-skill_0").text(lang["experience"]["knowledge"]["complementary-skill"]["itens"][0]);
+  $("#_experience_knowledge_complementary-skill_1").text(lang["experience"]["knowledge"]["complementary-skill"]["itens"][1]);
+  $("#_experience_knowledge_complementary-skill_2").text(lang["experience"]["knowledge"]["complementary-skill"]["itens"][2]);
+  $("#_experience_knowledge_complementary-skill_3").text(lang["experience"]["knowledge"]["complementary-skill"]["itens"][3]);
+  $("#_experience_knowledge_complementary-skill_4").text(lang["experience"]["knowledge"]["complementary-skill"]["itens"][4]);
+  $("#_experience_knowledge_complementary-skill_5").text(lang["experience"]["knowledge"]["complementary-skill"]["itens"][5]);
+  $("#_technologies").text(lang["technologies"]);
+  $("#_footer").text(lang["footer"]);
+} 
+
+function executeChangeLang(lang){
+  if(lang.includes("pt")){
+    changeLanguage(pt_BR);
+  }else {
+    changeLanguage(en_US);
+  }
+}
+
+// SCRIPT =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+executeChangeLang(detectBrowserLanguage());
+
+$(".btn.pt_BR").click(function () {
+  changeLanguage(pt_BR);
+});
+
+$(".btn.en_US").click(function () {
+  changeLanguage(en_US);
+});
